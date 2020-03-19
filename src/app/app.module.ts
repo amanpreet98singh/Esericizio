9ba17a70bec72136c9a4c3b80e70f4012ae0f691
amanpreet98singh <57998412+amanpreet98singh@users.noreply.github.com>
2020-03-19 18:11:13 +0100

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameListComponent } from './component/game-list/game-list.component';
+import { GamesListComponent } from './components/games-list/games-list.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { EditGameComponent } from './components/edit-game/edit-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameListComponent,
+    GamesListComponent,
+    GameDetailComponent,
+    MenuComponent,
+    EditGameComponent
   ],
   imports: [
     BrowserModule
