9aa8866d9367623acdb63a84301d76ec9525925c
amanpreet98singh <57998412+amanpreet98singh@users.noreply.github.com>
2020-03-23 10:02:34 +0100

    create interface

---
+++ src/app/model/game-item.ts
@@ -0,0 +1,8 @@
+export interface GameItem {
+    id:number;
+    name:string;
+    desc:string;
+    genere:string;
+    prezzo:string;
+    rating:number;
+}
---
+++ src/app/model/menu-item.ts
@@ -0,0 +1,5 @@
+export interface MenuItem {
+    id:number;
+    desc:string;
+    sele:boolean;
+}
