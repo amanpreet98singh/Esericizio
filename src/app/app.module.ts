9b37bde465761a8339d999f44b0f4d8886d2544e
amanpreet98singh <57998412+amanpreet98singh@users.noreply.github.com>
2020-03-23 11:50:32 +0100

    creazione menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { GameListComponent } from './component/game-list/game-list.component';
 import { GamesListComponent } from './components/games-list/games-list.component';
 import { GameDetailComponent } from './components/game-detail/game-detail.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -13,7 +12,6 @@ import { EditGameComponent } from './components/edit-game/edit-game.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    GameListComponent,
     GamesListComponent,
     GameDetailComponent,
     MenuComponent,
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MenuItem } from 'src/app/model/menu-item';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  @Output()
+  selectMenuItem: EventEmitter<number> = new EventEmitter<number>();
+  
+  selectedComponenet(id: number){
+    for(let menuItem of this.menuList){
+        menuItem.sele = id === menuItem.id
+    }
+  
+  this.selectMenuItem.emit(id);
 
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  menuList:MenuItem[]=[  
+  { id:1, 
+    desc:"Home",
+    sele: true},     
+     { id:2, desc:"Lista giochi", 
+     sele: false},     
+     { id:3, desc:"Modifica giochi", 
+     sele: false}   ]
 }
+
