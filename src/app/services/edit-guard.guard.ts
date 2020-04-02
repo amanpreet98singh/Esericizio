749b330a248bb0b1ae127b1e9ae177ed2f110969
amanpreet98singh <57998412+amanpreet98singh@users.noreply.github.com>
2020-04-02 11:04:48 +0200

    Create edit guard

---
+++ src/app/services/edit-guard.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditGuardGuard implements CanActivate {
+  constructor(private router:Router ){}
+  canActivate() {​
+    if(sessionStorage.getItem('login')==="admin"){
+      return true;​
+    }
+    else{
+      this.router.navigateByUrl('/Home');
+      return false;
+    }
+  }
+  
+}
