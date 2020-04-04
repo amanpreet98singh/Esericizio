ebc3adfc75e5924bfe575bd009df1dd6ce8bacce
amanpreet98singh <57998412+amanpreet98singh@users.noreply.github.com>
2020-04-04 11:25:25 +0200

    cambiato template form

--- src/app/components/forms/template-form/template-form.component.ts
+++ src/app/components/forms/template-form/template-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-template-form',
@@ -7,9 +8,10 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./template-form.component.scss']
 })
 export class TemplateFormComponent implements OnInit {
-  constructor(private loginServices: LoginService) { }
 
-  // loginList: { name: string, power: string }[] = [];
+  constructor(private utList: LoginService, private router:Router) { }
+
+  //loginList: { name: string, power: string }[] = [];
 
   model: { name: string, password: string } = {
     name: '',
@@ -18,4 +20,20 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+    control(ut:string, psw:string){
+      const control = this.utList.control(ut,psw);
+      if(control!='nope'){
+          sessionStorage.setItem('login', control);
+          sessionStorage.setItem('utente',ut);
+          alert("login effettuato "+control);
+          this.router.navigateByUrl('/Home');
+      }
+    else{
+        sessionStorage.removeItem('login');
+        sessionStorage.removeItem('utente');
+        alert("login errato");
+        this.router.navigateByUrl('/Login');
+      }
+    }
+  
 }
